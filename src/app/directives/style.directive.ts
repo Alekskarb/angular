915ad45a471a611353cafd62839f187e4a2f88a9
akorbutovski <akorbutovski@elinext.com>
2023-10-02 11:41:13 +0300

    feat: add events & dynamic handlersin directives (host-listener)

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,12 +1,24 @@
-import {Directive, ElementRef, Renderer2} from "@angular/core";
+import {Directive, ElementRef, HostListener, Renderer2} from "@angular/core";
 
 @Directive(
   {selector: '[appStyle]'}
 )
 
 export class StyleDirective {
-  constructor(private elRef: ElementRef, private  renderer: Renderer2 ) {
-    this.renderer.setStyle(elRef.nativeElement, 'color', 'blue')
-    // elRef.nativeElement.style.color = 'blue';
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {
+  }
+
+  // this.renderer.setStyle(elRef.nativeElement, 'color', 'blue')
+  // elRef.nativeElement.style.color = 'blue';
+  @HostListener('click', ['$event.target']) onClick(e: Event) {
+    console.log(e)
+  }
+
+  @HostListener('mouseenter') onEnter() {
+    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'blue')
+  }
+
+  @HostListener('mouseleave') onLeave() {
+    this.renderer.setStyle(this.elRef.nativeElement, 'color', null)
   }
 }
