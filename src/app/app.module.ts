44e4ec79b887fd1bb2b07126a71c0def10cf6254
Alex Korbutovski <akorbutovski@elinext.com>
2023-10-03 19:22:50 +0300

    feat: binding global & local services (@Injectable) providedIn root

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,19 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
-import {AppCounterService} from "./services/app-counter.service";
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CounterComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [AppCounterService],
+  // providers: [AppCounterService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/services/app-counter.service.ts
+++ src/app/services/app-counter.service.ts
@@ -1,11 +1,18 @@
+import {LogService} from "./log.service";
+import {Injectable} from "@angular/core";
+@Injectable({providedIn: 'root'})
 export class AppCounterService {
+  constructor(private service: LogService) {
+  }
   counter = 0;
 
   increase() {
+    this.service.log( "increase counter!")
     this.counter++
   }
 
   decrease() {
+    this.service.log( "decrease counter!")
     this.counter--
   }
 }
