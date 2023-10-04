0c895de2859091f8c2d3330be96b85dbcdb471e2
Alex Korbutovski <akorbutovski@elinext.com>
2023-10-04 14:12:31 +0300

    feat:  build forms, init, bind, validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  form!: FormGroup;
 
+  ngOnInit(): void {
+    this.form = new FormGroup<any>({
+      email: new FormControl('', [Validators.email, Validators.maxLength(11)]),
+      password: new FormControl(null, Validators.minLength(6))
+    })
+  }
+
+  submit() {
+    console.log('Form: ',this.form)
+    const dataForm = {...this.form.value}
+    console.log('Data Form: ', dataForm)
+  }
 }
 
