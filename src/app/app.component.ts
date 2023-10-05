62cf421e14f3112c24a24dfbb83db63dccef86a4
akorbutovski <akorbutovski@elinext.com>
2023-10-05 18:55:29 +0300

    feat: build http-server request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
 import {Component, OnInit} from '@angular/core'
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  appState = 'on'
-
-    handleChange() {
-        console.log(this.appState)
-    }
+export class AppComponent implements OnInit {
+constructor(private http: HttpClient) {
+}
+  ngOnInit() {
+this.http.get('https://jsonplaceholder.typicode.com/posts?_limit=2')
+    .subscribe(response => {
+      console.log(response)
+    })
+  }
 }
 
