ae5c81cac641aefe862fdaaf439325b176f8bc7c
akorbutovski <akorbutovski@elinext.com>
2023-10-05 17:27:43 +0300

    feat: implement OWN ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,56 +1,15 @@
 import {Component, OnInit} from '@angular/core'
-import { FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
-import {OwnValidators} from "./own.validators";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  form!: FormGroup;
-  // controls: Array<AbstractControl<any>> = []
-  ngOnInit(): void {
-    this.form = new FormGroup<any>({
-      email: new FormControl('', [OwnValidators.restrictedEmail, Validators.required , Validators.email, Validators.maxLength(11)],
-        OwnValidators.uniqEmail),
-      password: new FormControl(null, [Validators.required , Validators.minLength(6)]),
-      address: new FormGroup({
-        country: new FormControl('by', [Validators.required ]),
-        city: new FormControl('', Validators.required ),
-      }),
-      skills: new FormArray([])
-    })
-  }
+export class AppComponent {
+  appState = 'on'
 
-  submit() {
-    console.log('Form: ',this.form)
-    const dataForm = {...this.form.value}
-    console.log('Data Form: ', dataForm)
-    this.form.reset();
-  }
-
-  setCapital() {
-    const cityMap = {
-      ru: 'Moscow',
-      ua: 'Kiiv',
-      by: 'Belarus',
+    handleChange() {
+        console.log(this.appState)
     }
-    const key = this.form.get('address')!.get('country')?.value ?? 'ru'
-    // @ts-ignore
-    const city = cityMap[key]
-    this.form.patchValue({address: {city}})
-  }
-
-  addSkill() {
-    const control = new FormControl('', Validators.required);
-    // (<FormArray>this.form.get('skills')).push(control);
-    (this.form.get('skills') as FormArray).push(control);
-    // this.controls = (this.form.get('skills') as FormArray).controls
-  }
-
-  get userFormGroups () {
-    return this.form.get('skills') as FormArray
-  }
 }
 
