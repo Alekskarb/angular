876a85cec091b8f95a285475f9cf4c345c5da7dd
akorbutovski <akorbutovski@elinext.com>
2023-10-06 13:09:14 +0300

    feat: http-server requests response

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,27 @@
 import {Component, OnInit} from '@angular/core'
 import {HttpClient} from "@angular/common/http";
 
+export interface ToDos {
+  id?: number;
+  title: string;
+  completed: boolean
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-constructor(private http: HttpClient) {
-}
+  todos: ToDos[] = [];
+  constructor(private http: HttpClient) {
+  }
+
   ngOnInit() {
-this.http.get('https://jsonplaceholder.typicode.com/posts?_limit=2')
-    .subscribe(response => {
-      console.log(response)
-    })
+    this.http.get<ToDos[]>('https://jsonplaceholder.typicode.com/posts?_limit=2')
+      .subscribe(response => {
+        this.todos = response;
+      })
   }
 }
 
