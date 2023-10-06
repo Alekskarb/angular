6eaf81dedd9ab7a95014db232626c94aafc8d63c
akorbutovski <akorbutovski@elinext.com>
2023-10-06 13:38:11 +0300

    feat: method post request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,13 +14,31 @@ export interface ToDos {
 })
 export class AppComponent implements OnInit {
   todos: ToDos[] = [];
+  todoTitle = ''
+
   constructor(private http: HttpClient) {
   }
 
   ngOnInit() {
-    this.http.get<ToDos[]>('https://jsonplaceholder.typicode.com/posts?_limit=2')
+    this.http.get<ToDos[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
       .subscribe(response => {
         this.todos = response;
+      })
+  }
+
+  addTodo() {
+    if (!this.todoTitle.trim()) {
+      return
+    }
+    ;
+    const newTodo: ToDos = {
+      title: this.todoTitle,
+      completed: false
+    }
+    this.http.post<ToDos>('https://jsonplaceholder.typicode.com/todos', newTodo).subscribe(
+      (todo) => {
+        this.todos.push(todo)
+this.todoTitle=''
       })
   }
 }
