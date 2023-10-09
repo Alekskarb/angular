9bf953990e637f2ff11308b78fb7ca3c7f6d2a32
akorbutovski <akorbutovski@elinext.com>
2023-10-09 19:22:01 +0300

    feat: pass query params through html, active-route, router & fragment

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,11 +1,29 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
 import {PostsService} from '../posts.service'
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss']
 })
-export class PostsComponent {
-  constructor(public postsService: PostsService) {}
+export class PostsComponent implements OnInit{
+  showIds = false
+  constructor(public postsService: PostsService, public router: Router,
+              public route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      console.log(params)
+      this.showIds = !!params['showIds']
+    })
+  }
+
+  showIdsProgram() {
+    this.router.navigate(['/posts'], {
+      queryParams: {
+        showIds: true
+      }
+    })
+  }
 }
