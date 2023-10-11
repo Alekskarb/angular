de8d42f79592c9c6b9d4d7c354091b364e48869d
akorbutovski <akorbutovski@elinext.com>
2023-10-11 15:04:29 +0300

    feat: implement lazy-loading strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import {NgModule} from '@angular/core'
-import {RouterModule} from '@angular/router'
+import {PreloadAllModules, RouterModule, Routes, withPreloading} from '@angular/router'
 import {HomePageComponent} from './home-page/home-page.component'
 import {AboutPageComponent} from './about-page/about-page.component'
 import {AboutExtraPageComponent} from './about-page/about-extra-page/about-extra-page.component'
 
+const routes: Routes = [
+  {path: '', component: HomePageComponent, pathMatch: 'full'},
+  {
+    path: '', loadChildren: () => import('./about-page/about-page.module').then(mod => mod.AboutPageModule),
+  }
+]
+
 @NgModule({
-  imports: [RouterModule.forRoot([
-    {path: '', component: HomePageComponent, pathMatch: 'full'}
-  ])],
+  imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
