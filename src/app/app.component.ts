44c101cfc60105d05deb44cc687a5919f95e9b59
akorbutovski <akorbutovski@elinext.com>
2023-10-12 11:41:49 +0300

    feat: set new title & metaData (metatags)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, ComponentFactoryResolver, ViewChild, ViewContainerRef} from '@angular/core'
 import {ModalComponent} from "./modal/modal.component";
 import {RefDirective} from "./ref.directive";
+import {Meta, Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -12,9 +13,17 @@ export class AppComponent {
   @ViewChild(RefDirective) refDir?: RefDirective
 
   constructor(
-    private factory: ViewContainerRef
-    // private resolver: ComponentFactoryResolver,
+    private factory: ViewContainerRef,
+    private title: Title,
+    private meta: Meta,
   ) {
+    const ti = title.getTitle();
+    console.log(ti);
+    title.setTitle('New Tittle!')
+    meta.addTags([
+      {name: 'keywords', content: 'angular, google'},
+      {name: 'description', content: 'this id angular app'},
+    ])
   }
 
   showModal() {
